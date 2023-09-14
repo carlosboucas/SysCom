45c9d148c59c4975db4a7095a7935f57bc80b497
Carlos Rodrigo Bouças <carlos.rodrigo.cmb@gmail.com>
2023-09-14 09:52:19 -0300

    commit inicial

---
+++ DAL/Conexao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class Conexao
+    {
+        public static string Cn = "Data Source = LAPTOP-EPHHQPAI; Initial Catalog = dbcomercio; Integrated Security = true";
+    }
+}
